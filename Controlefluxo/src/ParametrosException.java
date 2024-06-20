b6b2c251843b9f835eccd0c34ee86744760d43a8
Gabicode123 <gabriel.bombardino@gmail.com>
2024-06-20 20:00:05 -0300

    ControleFluxo

---
+++ Controlefluxo/src/ParametrosException.java
@@ -0,0 +1,7 @@
+public class ParametrosException {
+    class ParametrosException extends Exception {
+
+        public ParametrosException (String message) {
+            
+            super(message);
+}
